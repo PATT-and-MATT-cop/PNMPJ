a7c424c0fcb64c2ad6992f1335e878f99ef485e1
harp515 <128121328+harp515@users.noreply.github.com>
2023-05-12 10:39:03 +0900

    :player:
    3인칭 시점

--- command W/Assets/Script/player.cs
+++ command W/Assets/Script/player.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    [SerializeField]
+    private Transform cameraArm;
+
     public float speed;
     public float jump;
     
     float hAxis;
     float vAxis;
+    public float turnspeed;
 
     bool isJump;
 
@@ -23,26 +27,54 @@ public class player : MonoBehaviour
     }
     void Update()
     {
-        GetInput();
+        LookAround();
         Move();
+        GetInput();
         Jump();
+    }
+    //3인칭 카메라
+    void Move()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isMove = moveInput.magnitude != 0;
+
+        if (isMove)
+        {
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            transform.position += moveDir * Time.deltaTime * 5f;
+        }
+    }
+    void LookAround()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector3 camAngle = cameraArm.rotation.eulerAngles;
+        float x = camAngle.x - mouseDelta.y;
+
+        if (x < 180f)
+        {
+            x = Mathf.Clamp(x, -1f, 70f);
+        }
+        else
+        {
+            x = Mathf.Clamp(x, 335f, 361f);
+        }
+        
+        cameraArm.rotation = Quaternion.Euler(camAngle.x - mouseDelta.y, camAngle.y + mouseDelta.x * turnspeed, camAngle.z);
+
     }
     void GetInput()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
-        jDown = Input.GetButtonDown("Jump");
-    }
-    void Move()
-    {
-        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
-
-        transform.position += moveVec * speed * 1f * Time.deltaTime;
+        jDown = Input.GetButton("Jump");
     }
     void Jump()
     {
-        if(jDown) {
-            rigid.AddForce(Vector3.up * 15,ForceMode.Impulse);
+        if(jDown && !isJump) {
+            rigid.AddForce(Vector3.up * jump,ForceMode.Impulse);
             isJump = true;
         }
     }
